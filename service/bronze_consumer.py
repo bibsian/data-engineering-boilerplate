99a9e0dcbaf621fce03f85ca57d6272997fe972e
Andrew Bibian <bibsian@users.noreply.github.com>
2026-01-16 14:23:33 -0800

    Scaffold the data lake consumer

--- service/bronze_consumer.py
+++ service/bronze_consumer.py
@@ -1,10 +1,12 @@
+import datetime
+import io
 import os
 import json
 import logging
 import time
 
-import boto3
-from kafka_client import create_consumer
+from kafka import KafkaConsumer
+
 
 logger = logging.getLogger(__name__)
 
@@ -12,17 +14,26 @@ STAGE = os.getenv("STAGE", "dev")
 
 
 class BronzeConsumerBase:
-    def __init__(self, s3_client, bucket, topic, batch_size=1000, t_limit=60):
+    def __init__(self, s3_client
+                 ,bucket
+                 ,consumer:KafkaConsumer
+                 ,batch_size=1000
+                 ,t_limit=60):
         self.s3_client = s3_client
         self.bucket = bucket
-        self.topic = topic
+        self.consumer = consumer
+        self.topic_name = self.consumer.topics()[0]
         self.batch_size = batch_size
         self.t_limit = t_limit
         self.last_flush = time.time()
         self.buffer = []
 
-    def consume(self, message):
-        pass # Add to buffer
+    def consume_batch(self, message):
+        for message in self.consumer:
+            self.buffer.append(message.value)
+            if self.should_flush():
+               self.flush()
+               self.last_flush = time.time()
 
     def should_flush(self):
         "Check batch size or Time Limit"
@@ -31,8 +42,18 @@ class BronzeConsumerBase:
         return size_reached or time_reached
 
     def flush(self):
-        self.last_flush = time.time()
-        pass # Write buffer to MinIO, clear buffer
+        """ Write buffered messages to MinIO S3 """
+        batch_data = json.dumps(self.buffer, indent=2).encode('utf-8')
+        self.s3_client.put_object(
+            bucket_name=self.bucket,
+            object_name=self.generate_key(),
+            data=io.BytesIO(batch_data),
+            length=len(batch_data)
+        )
+        self.buffer = []  # Clear buffer after flushing
 
     def generate_key(self):
-        pass # S3 object key (path) for the batch
+        timestamp = datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
+        object_name = (
+            f"bronze/{STAGE}/{self.topic_name}/batch_{timestamp}.json")
+        return object_name
