c53b7e80fe075ab93b250a96c36422cf3d94692e
Andrew Bibian <bibsian@users.noreply.github.com>
2026-02-20 20:44:04 -0800

    Typo

--- service/lake.py
+++ service/lake.py
@@ -5,7 +5,7 @@ import io
 def create_lake_client():
     """ Create and return a MinIO client """
     client = Minio(
-        "minio:9000",,
+        "minio:9000",
         access_key="minioadmin",
         secret_key="minioadmin",
         secure=True
